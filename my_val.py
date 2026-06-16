6c0f43a714d5f6acc38e943985d1db19163d1c06
UltralyticsAssistant <web@ultralytics.com>
2026-06-16 07:49:23 +0000

    Auto-format by https://ultralytics.com/actions

--- my_val.py
+++ my_val.py
@@ -1,15 +1,10 @@
 from ultralytics import YOLO
+
 # 加载训练好的最佳权重
 model = YOLO("runs/detect/train-2/weights/best.pt")
 # 验证
 metrics = model.val(
- data="coco8.yaml",
- imgsz=640,
- batch=2,
- device="cpu",
- project="runs/detect",
- name="val",
- exist_ok=True
+    data="coco8.yaml", imgsz=640, batch=2, device="cpu", project="runs/detect", name="val", exist_ok=True
 )
 # 打印核⼼指标
 print("验证集精确率 mAP50:", metrics.box.map50)
